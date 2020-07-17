86fd35edaa0d6831b859397cc5a425e2d389088d
kitchWWW <brian.e2014@gmail.com>
2020-07-17 04:10:59 -0600

    add comments

--- sosimidirunner.py
+++ sosimidirunner.py
@@ -38,39 +38,47 @@ available_ports = midiout.get_ports()
 # find all availiable output ports and open a connection to all of them
 print(available_ports)
 allPorts = []
-
 if available_ports:
 	for i in range(len(available_ports)):
 		outputPort = rtmidi.MidiOut()
 		outputPort.open_port(i)
 		allPorts.append(outputPort)
 
+# now start the big loop
 with midiout:
 	while True:
 
+		# request and parse the json file from the url
 		f = urllib.request.urlopen(url)
 		res = f.read().decode('utf-8')
 		positionData = json.loads(res)
+
+		# pull the data we want out of the midi file
 		dp1 = round(positionData['left_elbow_angle'])
 		dp2 = round(positionData['right_elbow_angle'])
 		dp3 = round(positionData['left_arm_angle'])
 		dp4 = round(positionData['right_arm_angle'])
 		
+		# print it for good measure so we can see what is going on
 		print(dp1);
 		print(dp2);
 		print(dp3);
 		print(dp4);
 
+		# plop it all into a list
 		controlUpdate = [
-			[176,14,dp1],
-			[176,15,dp2],
+			[176,14,dp1], # in this message, 176 specifies control data,
+			[176,15,dp2], # and 14-17 represents the control channel
 			[176,16,dp3],
 			[176,17,dp4]
 		]
+		# for each message in the list, send it.
 		for message in controlUpdate:
 			if sendToOne:
+				# if we only are sending it to the first availiable port
 				allPorts[0].send_message(message)
 			else:
+				#else, we loop through all ports and send the message to all of them.
 				for outputPort in allPorts:
 					outputPort.send_message(message)
 		time.sleep(0.05)
